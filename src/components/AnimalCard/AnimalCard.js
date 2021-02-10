2f3c07b566c8d99234173e61c749d7d964234cfc
gingkob <malivlada@hotmail.com>
2021-02-10 01:06:18 +0100

    final

--- src/components/AnimalCard/AnimalCard.js
+++ src/components/AnimalCard/AnimalCard.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card';
 import AnimalDetails from '../AnimalDetails/AnimalDetails';
 
 const AnimalCard = ({ name, size, ...props }) => {
   return (
-    <div style={{ textAlign: "center", display: 'flex', flexDirection: "column", width: '30%', backgroundColor: 'gold', borderRadius: "4px" }}>
-      <h3>{name}</h3>
-      <div>{size} kg</div>
-      <AnimalDetails {...props} />
-    </div>
+    <>
+      {/* <div style={{ textAlign: "center", display: 'flex', flexDirection: "column", width: '30%', backgroundColor: 'gold', borderRadius: "4px" }}>
+        <h3>{name}</h3>
+        <div>{size} kg</div>
+        <AnimalDetails {...props} />
+      </div> */}
+      <Card
+        title="Animal"
+        details={
+          <AnimalDetails
+            {...props}
+          />
+        }>
+        <h3>{name}</h3>
+        <div>{size}kg</div>
+      </Card>
+    </>
   )
 }
 
